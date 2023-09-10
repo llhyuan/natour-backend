ecb330fe4fbf8628b1a68ae19a5d07e25b555e97
llhyuan <lhyuan.liu21@icloud.com>
2023-09-10 10:20:15 +0200

    debugging delopyment

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 import connectToDatabase from './src/services/database.service';
 import app from './src/app';
-//import { Server } from 'http';
-import { createServer } from 'https';
-import fs from 'fs';
+import { Server } from 'http';
+// import { createServer } from 'https';
+// import fs from 'fs';
 
 require('dotenv').config();
 
 process.on('uncaughtException', (err) => {
   console.log('Uncaught Exception! Shutting down...');
   console.log(err);
-  httpsServer.close(() => {
+  server.close(() => {
     process.exit(1);
   });
 });
@@ -17,34 +17,29 @@ process.on('uncaughtException', (err) => {
 async function run(): Promise<void> {
   // Connecting to the database
   await connectToDatabase();
-  httpsServer.listen(PORT, () => {
-    console.log(`Secure server started on port ${PORT}`);
-  });
+  // httpsServer.listen(PORT, () => {
+  //   console.log(`Secure server started on port ${PORT}`);
+  // });
 }
 
 run().catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 3000;
 
-const options = {
-  key: fs.readFileSync(`${__dirname}/src/cert/key.pem`),
-  cert: fs.readFileSync(`${__dirname}/src/cert/cert.pem`),
-};
+const server: Server = app.listen(PORT, () => {
+  console.log(`Server is now listening on port ${PORT}`);
+});
+// const options = {
+//   key: fs.readFileSync(`${__dirname}/src/cert/key.pem`),
+//   cert: fs.readFileSync(`${__dirname}/src/cert/cert.pem`),
+// };
 
-const httpsServer = createServer(options, app);
-
-// httpsServer.listen(PORT, () => {
-//   console.log(`Secure server started on port ${PORT}`);
-// });
-
-// const server: Server = app.listen(PORT, () => {
-//   console.log(`Server is now listening on port ${PORT}`);
-// });
+//const httpsServer = createServer(options, app);
 
 process.on('unhandledRejection', (err) => {
   console.log(err);
   console.log('Unhandled rejection. Shuting down the server.');
-  httpsServer.close(() => {
+  server.close(() => {
     process.exit(1);
   });
 });
